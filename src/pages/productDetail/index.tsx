0c1f34b0cefc0ea939040b2ae342eabc2cee3214
Kayo Elias <contatodevkayo@gmail.com>
2022-03-24 00:14:46 -0300

    Detalhes do Produto

--- src/pages/productDetail/index.tsx
+++ src/pages/productDetail/index.tsx
@@ -1,13 +1,67 @@
+import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Header } from "../../components/Header";
-
+import { api } from "../../services/api";
+import { Container, ContentArea, ButtonsOrderDetailProduct } from "./style";
+import { toBrlNumber } from "../../utils/number";
+import { FaMinus, FaPlus } from "react-icons/fa";
+interface productProps {
+  coffeDescription: string;
+  coffeImage: string;
+  coffeName: string;
+  price: number;
+}
 export function ProductDetail() {
+  const [productDetail, setProductDetail] = useState<productProps>({
+    coffeDescription: "",
+    coffeImage: "",
+    coffeName: "",
+    price: 0,
+  });
+  const [loading, setLoading] = useState(true);
   let navigate = useNavigate();
   let params = useParams();
+
+  useEffect(() => {
+    api.get(`/coffes`).then((response) => {
+      const filtro = response.data.find(
+        (object: any) => object.id === Number(params.productId)
+      );
+
+      setProductDetail(filtro);
+
+      setLoading(false);
+    });
+
+    console.log(!loading && productDetail);
+  }, []);
+
   return (
     <>
       <Header />
-      <h1>{params.productId}</h1>
+      <Container>
+        <ContentArea>
+          <h1>{productDetail.coffeName}</h1>
+          <h2>{toBrlNumber(productDetail.price)}</h2>
+          <p>{productDetail.coffeDescription}</p>
+          <ButtonsOrderDetailProduct>
+            <button>Order</button>
+            <button className="quantityButton">
+              <FaMinus />
+            </button>
+            <button className="quantityButton">
+              <FaPlus />
+            </button>
+          </ButtonsOrderDetailProduct>
+        </ContentArea>
+        <div>
+          <img
+            src={productDetail.coffeImage}
+            alt={productDetail.coffeName}
+            title={productDetail.coffeName}
+          />
+        </div>
+      </Container>
     </>
   );
 }
--- src/pages/productDetail/style.ts
+++ src/pages/productDetail/style.ts
@@ -1,1 +1,78 @@
-import styled from 'styled-components'
+import styled, { keyframes } from "styled-components";
+import { fadeInLeft, fadeInRight } from "react-animations";
+const fadeAnimationLeft = keyframes`${fadeInLeft}`;
+const fadeAnimationRight = keyframes`${fadeInRight}`;
+
+export const Container = styled.div`
+  max-width: 1120px;
+  margin: 2rem auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+
+  div + div {
+    height: 20rem;
+    display: flex;
+    justify-content: center;
+    align-items: start;
+    color: #000;
+    animation: 2s ${fadeAnimationRight};
+
+    img {
+      max-height: 100%;
+      max-width: 100%;
+      object-fit: cover;
+    }
+  }
+`;
+
+export const ContentArea = styled.div`
+  animation: 1s ${fadeAnimationLeft};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  h1 {
+    font-size: 4rem;
+    color: var(--textLight);
+  }
+
+  h2 {
+    font-size: 3rem;
+    color: var(--green);
+    margin: 0.8rem 0;
+  }
+
+  p {
+    font-size: 1.5rem;
+    max-width: 80%;
+    text-align: justify;
+  }
+`;
+
+export const ButtonsOrderDetailProduct = styled.div`
+  display: flex;
+
+  width: 100%;
+  gap: 2rem;
+  align-content: space-around;
+  margin: 2rem 0;
+
+  button {
+    padding: 1rem 0.9rem;
+    border: 0;
+    color: #fff;
+    background-color: var(--green);
+    font-size: 1rem;
+    text-transform: uppercase;
+    border-radius: 6px;
+    transition: filter 0.2s;
+    transition: background 0.2s;
+    &:hover {
+      filter: brightness(0.9);
+    }
+
+    &.quantityButton {
+      background-color: var(--textLight);
+    }
+  }
+`;
