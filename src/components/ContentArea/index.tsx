c2066533b88a829e2a322bb1aa052142028ef6ea
Kayo Elias <contatodevkayo@gmail.com>
2022-03-25 16:43:22 -0300

    ,

--- src/components/ContentArea/index.tsx
+++ src/components/ContentArea/index.tsx
@@ -1,14 +1,16 @@
-import { Container, Content } from "./styles";
-import { CoffesInformations } from "../CoffesInformations";
 import { AllProducts } from "../AllProducts";
+import { CoffesInformations } from "../CoffesInformations";
+import { Container, Content } from "./styles";
 
 export function ContentArea() {
   return (
-    <Container>
-      <Content>
-        <CoffesInformations />
-      </Content>
-      <AllProducts />
-    </Container>
+    <>
+      <Container>
+        <Content>
+          <CoffesInformations />
+        </Content>
+        <AllProducts />
+      </Container>
+    </>
   );
 }
