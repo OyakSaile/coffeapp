f895e64d9be796b8d6b004077bff6c0d504c9fcd
Kayo Elias <contatodevkayo@gmail.com>
2022-03-25 16:43:02 -0300

    pequenas animações de teste

--- src/components/CoffesInformations/styles.ts
+++ src/components/CoffesInformations/styles.ts
@@ -1,6 +1,7 @@
-import styled, { keyframes } from 'styled-components';
-import { fadeIn } from 'react-animations';
 import { darken, transparentize } from "polished";
+import { fadeIn } from "react-animations";
+import styled, { keyframes } from "styled-components";
+
 const fadeAnimation = keyframes`${fadeIn}`;
 
 export const Informations = styled.div`
@@ -73,57 +74,6 @@ export const ImageContainer = styled.div`
   button {
     background-color: transparent;
     border: 0;
-  }
-`;
-
-export const Container = styled.form`
-  h2 {
-    color: var(--text-title);
-    font-size: 1.5rem;
-    margin-bottom: 2rem;
-  }
-
-  input {
-    width: 100%;
-    padding: 0 1.5rem;
-    border-radius: 0.25rem;
-    height: 4rem;
-    background-color: #e7e9ee;
-    border: 1px solid #d7d7d7;
-    font-weight: 400;
-    font-size: 1rem;
-    &::placeholder {
-      color: var(--text-body);
-    }
-    & + input {
-      margin-top: 1rem;
-    }
-  }
-
-  h2 {
-    font-weight: 400;
-    span {
-      color: var(--green);
-      font-weight: bold;
-    }
-  }
-
-  button[type="submit"] {
-    width: 100%;
-    padding: 0 1.5rem;
-    height: 4rem;
-    background-color: var(--green);
-    border-radius: 0.25rem;
-    font-size: 1rem;
-    border: 0;
-    color: #fff;
-    margin-top: 1.5rem;
-    font-weight: 600;
-    transition: filter 0.2s;
-
-    &:hover {
-      filter: brightness(0.9);
-    }
   }
 `;
 
