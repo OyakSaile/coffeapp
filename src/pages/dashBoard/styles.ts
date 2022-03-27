2c9abcecf69eecf58af8a734b2c21ea824f56003
Kayo Elias <contatodevkayo@gmail.com>
2022-03-27 18:29:47 -0300

    dashboard

--- src/pages/dashBoard/styles.ts
+++ src/pages/dashBoard/styles.ts
@@ -1,22 +1,86 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  min-height: 100vh;
+  height: 100vh;
   width: 100%;
-
   display: flex;
 `;
 
-export const Menu = styled.header`
+export const ContentDashBoardArea = styled.div`
+  flex: 1;
+  margin: 2rem auto;
+
+  gap: 3rem;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+
+  overflow-y: scroll;
+  scroll-behavior: smooth;
+
+  margin: 2rem;
+  h2 {
+    color: var(--textLight);
+    font-size: 2.5rem;
+  }
+`;
+
+export const CardDashBoard = styled.div`
+  img {
+    width: 5rem;
+    margin: 1.5rem 0;
+  }
+  background-color: #fff;
+  width: 25rem;
+  height: 25rem;
+  padding: 2rem;
+  border-radius: 2rem;
   display: flex;
   justify-content: center;
-  background: #ffffff;
-  box-shadow: 4px 0px 40px rgba(0, 0, 0, 0.25);
-  width: 7rem;
-  display: flex;
-  padding: 1rem;
+  align-items: flex-start;
+  flex-direction: column;
+
+  position: relative;
 `;
 
-export const ContentDashBoardArea = styled.header`
-  flex: 1;
+export const Icons = styled.div`
+  margin: 1.5rem 0;
+`;
+export const IconWithText = styled.div`
+  display: flex;
+  text-align: left;
+  gap: 0.8rem;
+
+  color: var(--textLight);
+
+  svg {
+    color: #4285f4;
+  }
+`;
+export const CheckButtons = styled.div`
+  display: flex;
+  align-items: center;
+  text-align: left;
+  font-size: 2rem;
+  gap: 1.5rem;
+
+  button {
+    background-color: transparent;
+    border: 0;
+    font-size: 2rem;
+
+    svg {
+      transition: filter 0.2s;
+      &.orderDone {
+        color: #77f899;
+      }
+      &.recuseOrder {
+        color: #fc7b7b;
+      }
+
+      &:hover {
+        filter: brightness(0.8);
+      }
+    }
+  }
 `;
